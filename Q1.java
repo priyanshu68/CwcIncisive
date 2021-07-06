c24f2ec476ed4c9e0b7b36ef770f6c57cb22b8a1
priyanshu68 <86975736+priyanshu68@users.noreply.github.com>
2021-07-06 14:24:42 +0530

    Add files via upload

---
+++ Q1.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+public class Q1 {
+
+	public static void main(String[] args) {
+		int n ;
+		Scanner sc = new Scanner (System.in);
+		System.out.println("Enter the number of elements :");
+		n = sc.nextInt();
+		int [] array = new int[n];
+		System.out.println("Enter the elements of array :");
+		for(int i=0;i<n;i++)
+		{
+			array [i] = sc.nextInt();
+		}
+		System.out.println("The elements  array  are :");
+		for (int i = 0;i<n;i++)
+		{
+			System.out.println(array[i]);
+		}
+		
+		
+		int count =0 ;
+			  for ( int i = 0; i <array.length - 1;i++)
+			  { 
+				  int minInd = i; 
+				  for (int j=i;j<array.length;j++) 
+				  { 
+					  if ( array[j]<array[minInd] ) 
+					  { 
+						  minInd = j;
+						  count++;
+						  } 
+					  }
+			 
+			 int temp = array[i]; 
+			 array[i]= array[minInd];
+			 array[minInd]= temp;
+			  
+			   }
+		System.out.println();	
+      System.out.println(count);
+      
+	}
+}
+
+
