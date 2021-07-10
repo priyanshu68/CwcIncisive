7f2335ce2b4b5f172bfc291922145040b18ff90f
priyanshu68 <86975736+priyanshu68@users.noreply.github.com>
2021-07-10 21:54:12 +0530

    Add files via upload

---
+++ Q8.java
@@ -0,0 +1,10 @@
+
+public class Q8 {
+
+	public static void main(String[] args) {
+		float x;
+		x = (((-2)*2)-3)/5f;
+		System.out.println("The solution of the equation -2*2=5x+3 is =" +x);
+	}
+
+}
