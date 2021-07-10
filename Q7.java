b3672c597cc2eeab407cd1e4eaa79fa9c45f0609
priyanshu68 <86975736+priyanshu68@users.noreply.github.com>
2021-07-10 21:30:47 +0530

    Add files via upload

---
+++ Q7.java
@@ -0,0 +1,10 @@
+
+public class Q7 {
+
+	public static void main(String[] args) {
+		int n,nthTerm = 78 ,a = 3,d = 5;
+		n = ((nthTerm - a)/d) + 1;
+		System.out.println(" the value of 'i' where the output is 78 is :" +n );
+	}
+
+}
